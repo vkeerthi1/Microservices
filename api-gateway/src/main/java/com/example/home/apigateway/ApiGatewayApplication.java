dff4ac7c0ad93f89f83032543daf7d27ddab9c19
keerthi v <91740419+vkeerthi1@users.noreply.github.com>
2021-10-05 09:41:12 +0530

    Add files via upload

---
+++ api-gateway/src/main/java/com/example/home/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.example.home.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+//@EnableHystrix
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
